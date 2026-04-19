735c7fccb42b69fe5617b1e5571e62edcc377ff2
yuvrajangadsingh <yuvrajangad.s@gmail.com>
2026-04-20 00:06:01 +0530

    v0.3.0

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,7 +6,7 @@ def test_version():
     runner = CliRunner()
     result = runner.invoke(cli, ["--version"])
     assert result.exit_code == 0
-    assert "0.2.0" in result.output
+    assert "0.3.0" in result.output
 
 
 def test_help():
